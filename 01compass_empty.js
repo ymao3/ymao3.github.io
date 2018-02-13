b21ceba908b25b43b7637dcd5c95344a19bec3c7
Apple <apple@zacary.local>
2018-02-13 14:29:32 -0600

    dirction

--- 01compass_empty.js
+++ 01compass_empty.js
@@ -9,7 +9,19 @@ oriented = function(e) {
         }
         
         if(compassHeading >315 || compassHeading < 45){
-            text_area.textContent = compassHeading;
+            text_area.textContent = "north";
+        }
+
+        if(compassHeading >45 && compassHeading < 135){
+            text_area.textContent = "east";
+        }
+
+        if(compassHeading >135 && compassHeading < 225){
+            text_area.textContent = "south";
+        }
+
+        if(compassHeading >225 && compassHeading < 315){
+            text_area.textContent = "west";
         }
 
          
