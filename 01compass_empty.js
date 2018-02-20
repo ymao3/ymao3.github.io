5eb7dae681fa33da14725a46d6caba35f050ca54
zacary <2433667425@qq.com>
2018-02-20 10:30:10 -0600

    Initial commit

--- 01compass_empty.js
+++ 01compass_empty.js
@@ -8,29 +8,30 @@ oriented = function(e) {
             compassHeading = e.alpha;
         }
         
-        if(compassHeading >315 || compassHeading < 45){
+        if(compassHeading >330 || compassHeading < 45){
             text_area.textContent = "-----------NORTH----------------------ss-----------------------uuuu---------------------f-ff-f-------------------f--ff--f-----------------e---ee---e---------------r----rr----r-------------------ii------------------------nn------------------------gg------------------------ii------------------------nn------------------------gg------------------------";
         }
-        if(compassHeading >345 || compassHeading < 75){
+        
+        if(compassHeading >45 && compassHeading < 90){
             text_area.textContent = "娓尯灏忓鏍";
         }
 
-        if(compassHeading >45 && compassHeading < 135){
+        if(compassHeading >65 && compassHeading < 110){
             text_area.textContent = "-----------EAST-----------------------ii-----------------------ssss---------------------o-oo-o-------------------l--ll--l-----------------a---aa---a---------------t----tt----t-------------e-----ee-----e------------------ii------------------------nn------------------------gg------------------------ii------------------------nn------------------------gg------------------------";
         }
-        if(compassHeading >75 && compassHeading < 165){
+        if(compassHeading >110 && compassHeading < 150){
             text_area.textContent = "SUNSET"
-        if(compassHeading >135 && compassHeading < 225){
+        if(compassHeading >150 && compassHeading < 200){
             text_area.textContent = "-----------SOUTH----------------------cc-----------------------aaaa---------------------l-ll-l-------------------m--mm--m---------------------mm------------------------ii------------------------nn------------------------gg------------------------ii------------------------nn------------------------gg------------------------";
         }
-        if(compassHeading >165 && compassHeading < 255){
+        if(compassHeading >200 && compassHeading < 250){
             text_area.textContent = "銇婂彴鍫存捣娴滃叕鍦�";
         }
 
-        if(compassHeading >225 && compassHeading < 315){
+        if(compassHeading >250 && compassHeading < 315){
             text_area.textContent = "-----------WEST-----------------------tt-----------------------hhhh---------------------i-ii-i-------------------n--nn--n-----------------k---kk---k--------------------ii------------------------nn------------------------gg------------------------ii------------------------nn------------------------gg------------------------";
         }
-        if(compassHeading >255 && compassHeading < 345){
+        if(compassHeading >315 && compassHeading < 330){
             text_area.textContent = "TOYKO TOWER";
         }
 
